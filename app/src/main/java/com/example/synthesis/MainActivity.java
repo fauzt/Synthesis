889930500acad4a3c02d21a018d25c5129fc1057
fauzt <fauzan.a.y@gmail.com>
2019-10-12 20:42:05 +0800

    fixed ui

--- app/src/main/java/com/example/synthesis/MainActivity.java
+++ app/src/main/java/com/example/synthesis/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.synthesis;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goDiscover() {
+        Intent intent =new Intent(MainActivity.this, CameraActivity.class);
+        startActivity(intent);
     }
 }
